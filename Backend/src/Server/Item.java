d9c8d02bdd57d56e0be5091395556e0763b6a898
Oscar Wong <wong.oscar67@gmail.com>
2019-04-12 13:43:55 -0600

    Update Item.java

--- Backend/src/Server/Item.java
+++ Backend/src/Server/Item.java
@@ -172,7 +172,7 @@ public class Item {
 	 */
 	public String toString () {
 		return "Item ID: " + itemId + "\tItem Name: " + itemName + "\tItem Quantity: " + 
-	           itemQuantity + "\n";
+	           itemQuantity;
 	}
 
 	/**
@@ -181,7 +181,7 @@ public class Item {
 	 */
 	public String toStringNoTabs() {
 		return "Item ID: " + itemId + " Item Name: " + itemName + " Item Quantity: " + 
-	           itemQuantity + "\n";
+	           itemQuantity;
 	}
 
 	/**
