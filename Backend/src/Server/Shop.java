89590ef71586b4d2d36453a5a928cb98d08ad918
Oscar Wong <wong.oscar67@gmail.com>
2019-04-12 13:12:58 -0600

    new shit

--- Backend/src/Server/Shop.java
+++ Backend/src/Server/Shop.java
@@ -7,12 +7,13 @@ import java.io.PrintWriter;
 import java.io.*;
 
 /**
- * Class to manage the Tool Shop Backend. 
+ * Class to manage the Tool Shop Backend.
+ * 
  * @author M. Moshirpour, Oscar Wong, Jeremy Olea.
  * @version 2.0
  * @since April 3rd, 2019
  */
-public class Shop implements Runnable{
+public class Shop implements Runnable {
 	/**
 	 * The Shop's Inventory.
 	 */
@@ -20,14 +21,14 @@ public class Shop implements Runnable{
 	/**
 	 * The Shop's list of Suppliers.
 	 */
-	private ArrayList <Supplier> supplierList;
+	private ArrayList<Supplier> supplierList;
 	/**
-    * Printing back to client
-    */
+	 * Printing back to client
+	 */
 	private PrintWriter socketOut;
-    /**
-     * Reading input from client
-     */
+	/**
+	 * Reading input from client
+	 */
 	private BufferedReader socketIn;
 	/**
 	 * Socket for client
@@ -38,26 +39,28 @@ public class Shop implements Runnable{
 	 * Database Controller.
 	 */
 	private DbController db;
-	
+
 	/**
 	 * Constructs a new Shop.
+	 * 
 	 * @param inventory Inventory for the Shop.
 	 * @param suppliers Suppliers to the Shop.
 	 */
-	public Shop (Socket s, Inventory inventory, ArrayList <Supplier> suppliers) {
-		clientSocket = s;		
+	public Shop(Socket s, Inventory inventory, ArrayList<Supplier> suppliers) {
+		clientSocket = s;
 		theInventory = inventory;
 		supplierList = suppliers;
 		try {
-            socketIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            socketOut = new PrintWriter(clientSocket.getOutputStream());
-        } catch(IOException e) {
-            e.printStackTrace();
-        }		
+			socketIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			socketOut = new PrintWriter(clientSocket.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * Link the database controller to the Shop.
+	 * 
 	 * @param db Database controller to set.
 	 */
 	public void setDbController(DbController db) {
@@ -76,109 +79,136 @@ public class Shop implements Runnable{
 	 */
 	public void communicate() {
 		String input = "";
-		while(true) {
-            try { 
-                input = socketIn.readLine();
+		while (true) {
+			try {
+				input = socketIn.readLine();
 				System.out.println(input);
 				refreshInventory();
-                if(input.equals("GET/TOOLS")) {
+				if (input.equals("GET/TOOLS")) {
 					sendString(theInventory.toString());
 					sendString("GET/TOOLS");
-				}
-				else if(input.startsWith("SEARCH/TOOL/NAME/")) {
+				} else if (input.startsWith("SEARCH/TOOL/NAME/")) {
 					String toolName = input.replace("SEARCH/TOOL/NAME/", "");
 					sendString(getItem(toolName));
-				}
-				else if (input.startsWith("SEARCH/TOOL/ID/")) {
+				} else if (input.startsWith("SEARCH/TOOL/ID/")) {
 					int toolId = Integer.parseInt(input.replace("SEARCH/TOOL/ID/", ""));
 					sendString(getItem(toolId));
-				}
-				else if (input.startsWith("GET/TOOL/QUANTITY/")) {
+				} else if (input.startsWith("GET/TOOL/QUANTITY/")) {
 					String toolName = input.replace("GET/TOOL/QUANTITY/", "");
 					sendString(getItemQuantity(toolName));
 				} else if (input.startsWith("DECREASE/TOOL/QUANTITY/")) {
 					String toolName = input.replace("DECREASE/TOOL/QUANTITY/", "");
 					sendString(decreaseItem(toolName));
+				} else if (input.startsWith("BUY/TOOL/NAME/")) {
+					String rest = input.replace("/BUY/TOOL/NAME/", "");
+					String[] inputs = rest.split("/");
+					String toolName = inputs[0];
+					int quantity = Integer.parseInt(inputs[1]);
+					sendString(buyItem(toolName, quantity));
 				}
-            } catch(IOException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 				close();
-            }           
-        }
+			}
+		}
 	}
 
 	/**
 	 * Send output to socket.
+	 * 
 	 * @param send String to send.
 	 */
 	public void sendString(String send) {
-        socketOut.println(send);
-        socketOut.flush();
-    }
+		socketOut.println(send);
+		socketOut.flush();
+	}
 
 	public void refreshInventory() {
-		//	update the inventory from the database
+		// update the inventory from the database
 		db.fetchSuppliers(supplierList);
 		theInventory = new Inventory(db.fetchItems(supplierList));
 	}
 
-    /**
-     * CLoses the socket, printwrite and bufferedreader
-     */
-    public void close() {
-        try {
-            socketIn.close();
-            socketOut.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-	
+	/**
+	 * CLoses the socket, printwrite and bufferedreader
+	 */
+	public void close() {
+		try {
+			socketIn.close();
+			socketOut.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	/**
 	 * Return the Shop's Inventory.
+	 * 
 	 * @return The Shop's Inventory.
 	 */
-	public Inventory getTheInventory () {
+	public Inventory getTheInventory() {
 		return theInventory;
 	}
 
 	/**
 	 * Set the Shop's inventory.
+	 * 
 	 * @param inventory The Shop's incoming Inventory.
 	 */
-	public void setTheInventory (Inventory inventory) {
+	public void setTheInventory(Inventory inventory) {
 		theInventory = inventory;
 	}
-	
+
 	/**
 	 * Return the Shop's list of Suppliers.
+	 * 
 	 * @return The Shop's list of Suppliers.
 	 */
-	public ArrayList<Supplier> getSupplierList (){
+	public ArrayList<Supplier> getSupplierList() {
 		return supplierList;
 	}
 
 	/**
 	 * Set the Shop's list of Suppliers.
+	 * 
 	 * @param suppliers The Shop's incoming list of Suppliers.
 	 */
-	public void setSupplierList (ArrayList <Supplier> suppliers){
+	public void setSupplierList(ArrayList<Supplier> suppliers) {
 		supplierList = suppliers;
 	}
-	
+
 	/**
 	 * List all items in the Shop.
 	 */
 	public void listAllItems() {
-		System.out.println(theInventory);		
+		System.out.println(theInventory);
+	}
+
+	/**
+	 * Buy an item.
+	 * @param toolName Name of item to buy.
+	 * @param quantity quantity to buy.
+	 * @return Info about whether or not the operation succeeded.
+	 */
+	public String buyItem(String toolName, int quantity) {
+		if (theInventory.getItemQuantity(toolName) >= quantity) {
+			for (int i = 0; i < quantity; i++) {
+				theInventory.manageItem(name, db);
+			}
+			return "Purchased!";
+		} else {
+			return "Sorry, you can't buy this.";
+		}
 	}
 
 	/**
 	 * Decrease quantity of an item in the Shop.
+	 * 
 	 * @param name Name of item to decrease.
-	 * @return String containing info about whether or not the transaction succeeded.
+	 * @return String containing info about whether or not the transaction
+	 *         succeeded.
 	 */
-	public String decreaseItem (String name) {
+	public String decreaseItem(String name) {
 		if (theInventory.manageItem(name, db) == null)
 			return "Couldn't not decrease item quantity!\n";
 		else
@@ -189,66 +219,71 @@ public class Shop implements Runnable{
 	 * List every Supplier to the Shop.
 	 */
 	public void listAllSuppliers() {
-		for (Supplier s: supplierList) {
+		for (Supplier s : supplierList) {
 			System.out.println(s);
 		}
-		
+
 	}
 
 	/**
 	 * Return an Item as a String, based on its name.
+	 * 
 	 * @param name Name of Item to get.
 	 * @return Item in question.
 	 */
 	public String getItem(String name) {
 		Item theItem = theInventory.searchForItem(name);
 		if (theItem == null)
-		     return "Item " + name + " could not be found!";
+			return "Item " + name + " could not be found!";
 		else
-			 return theItem.toStringNoTabs();
-			
+			return theItem.toStringNoTabs();
+
 	}
 
 	/**
 	 * Return an Item as a String, based on its ID.
+	 * 
 	 * @param id ID of Item to get.
 	 * @return Item in question.
 	 */
 	public String getItem(int id) {
 		Item theItem = theInventory.searchForItem(id);
 		if (theItem == null)
-		     return "Item number " + id + " could not be found!";
+			return "Item number " + id + " could not be found!";
 		else
-			return theItem.toStringNoTabs(); 
+			return theItem.toStringNoTabs();
 	}
-	
+
 	/**
 	 * Output an Item as a String.
+	 * 
 	 * @param theItem Item to output.
 	 * @return The Item as a String.
 	 */
-	private String outputItem (Item theItem){
+	private String outputItem(Item theItem) {
 		return "The item information is as follows: \n" + theItem;
 	}
 
 	/**
-	 * Return the quantity of a particular Item. 
+	 * Return the quantity of a particular Item.
+	 * 
 	 * @param name Name of Item to return the quantity of.
 	 * @return Quantity of Item with given name.
 	 */
 	public String getItemQuantity(String name) {
 		int quantity = theInventory.getItemQuantity(name);
 		if (quantity < 0)
-		    return "Item " + name + " could not be found!";
+			return "Item " + name + " could not be found!";
 		else
 			return "The quantity of Item " + name + " is: " + quantity + "\n";
 	}
 
 	/**
 	 * Return the Order currently held by the Shop's Inventory.
+	 * 
 	 * @return The Order in the Inventory, as a String.
 	 */
-	public String printOrder() {		
+	public String printOrder() {
 		return theInventory.printOrder();
 	}
 }
